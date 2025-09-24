254d448616b1d83e0ab94eeb28089d4cdcd683e4
Nathan Allan <114799638+nathall-byeworld@users.noreply.github.com>
2025-09-24 23:04:28 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/nathall-byeworld.github.io/dataosn/", // replace with your repo name
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
